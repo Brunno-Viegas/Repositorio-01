a6c9e2d235f09282ac9be69157eb2572e036a11a
Brunno-Viegas <2412brunnoviegas@gmail.com>
2023-10-25 07:52:30 -0300

    commit work - 20231025 : 0752

---
+++ Fontes C#/ParamsStudy/ParamsStudy/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ParamsStudy
+{
+    internal class Program {
+        static void Main(string[] args) {
+
+            /*
+            int s1 = Calculator.Sum(new int[] {1, 2});
+            int s2 = Calculator.Sum(new int[] { 1, 2, 3 });
+            */
+
+            int s1 = Calculator.Sum(1, 2);
+            int s2 = Calculator.Sum(1, 2, 3);
+
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+        }
+    }
+}
